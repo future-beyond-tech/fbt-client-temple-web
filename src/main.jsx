b808b3e31d8a8c5c8747dff7bfd5e037a98c9ff1
feroze <bashaferoz66@gmail.com>
2026-03-27 16:34:52 +0530

    Add Vercel Analytics integration for improved tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import TempleDonation from "./temple-donation/TempleDonation.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <TempleDonation />
+    <Analytics />
   </React.StrictMode>
 );
